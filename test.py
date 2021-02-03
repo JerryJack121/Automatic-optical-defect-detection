f4664303d75af971a5afb75be1ead4800fcca1f9
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2021-02-03 14:47:51 +0800

    Update

--- test.py
+++ test.py
@@ -17,29 +17,32 @@ from tqdm import tqdm
 
 device = torch.device("cuda")
 
-PATH_test = r'D:\dataset\automatic-optical-defect-detection\org\test_images'
-PATH_sample = r'D:\GitHub\Automatic-optical-defect-detection\results\upload_sample.csv'
+PATH_test = r'D:\dataset\automatic-optical-defect-detection\org\test_images'    # 測試資料集
+PATH_sample = r'D:\GitHub\Automatic-optical-defect-detection\results\upload_sample.csv' # 上傳範例檔案
 
 model = torchvision.models.resnet101(pretrained=False, progress=True)
 fc_features = model.fc.in_features
 model.fc = nn.Linear(fc_features, 6)
 model.to(device)
-model.load_state_dict(torch.load('./weights/0202/epoch9-loss0.0058640-val_loss0.0170710-acc0.9970.pth'))
+model.load_state_dict(torch.load('./weights/0202/epoch9-loss0.0058640-val_loss0.0170710-acc0.9970.pth'))    # 權重
 
 test_transforms = transforms.Compose([transforms.Resize((256, 256)),
                                        transforms.ToTensor(),
                                        transforms.Normalize([0.485, 0.456, 0.406],
                                                             [0.229, 0.224, 0.225])])
 
+# 讀取測試順序
 df = pd.read_csv(PATH_sample)
 img_list = []
 for index, row in df.iterrows():
     img = os.path.join(PATH_test, row['ID'])
     img_list.append(img)
 
+# 載入測試資料
 test_data = TestDataset(img_list, transform=test_transforms)
 test_loader = torch.utils.data.DataLoader(test_data, batch_size=64)
-   
+
+# 測試
 results = []                                     
 model.eval()         
 with torch.no_grad():                                 
@@ -50,5 +53,4 @@ with torch.no_grad():
         results = np.hstack((results, np.array(predict_y.cpu().detach())))
 
 df['Label'] = results
-
 df.to_csv('./results/submission.csv', index=None)
